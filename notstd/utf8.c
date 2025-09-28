a09581e7abac68f4b0a5d2b850e864b3c083f6c1
vbextreme <vbextreme@hotmail.com>
2025-09-28 19:25:53 +0200

    fix not check in validate utf8 size 0

--- notstd/utf8.c
+++ notstd/utf8.c
@@ -508,7 +508,7 @@ const utf8_t* utf8_find_ucs4_range(const utf8_t* u, size_t rangeSt, size_t range
 
 int ucs4_validate(ucs4_t u4){
 	utf8_t u8[5];
-	ucs4_to_utf8(u4, u8);
+	if( !ucs4_to_utf8(u4, u8) ) return 0;
 	return utf8_validate(u8);
 }
 
